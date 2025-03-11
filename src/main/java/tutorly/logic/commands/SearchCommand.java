36fdfa9a6fd4c2c3cb0b0fc09a59a76a804cee70
SociallyIneptWeeb <sociallyinept.csit@gmail.com>
2025-03-11 17:43:40 +0800

    Add name parameter for search command

--- src/main/java/tutorly/logic/commands/SearchCommand.java
+++ src/main/java/tutorly/logic/commands/SearchCommand.java
@@ -1,6 +1,7 @@
 package tutorly.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static tutorly.logic.parser.CliSyntax.PREFIX_NAME;
 
 import tutorly.commons.util.ToStringBuilder;
 import tutorly.logic.Messages;
@@ -17,8 +18,8 @@ public class SearchCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Searches for all students whose names contain any of "
             + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
-            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " alice bob charlie";
+            + "Parameters: " + PREFIX_NAME + "KEYWORD [KEYWORDS]...\n"
+            + "Example: " + COMMAND_WORD + " " + PREFIX_NAME + "alice bob charlie";
 
     private final NameContainsKeywordsPredicate predicate;
 
