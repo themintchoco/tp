1051486483bc64c168b032e3b6cb02069052bc4e
mint <76428902+themintchoco@users.noreply.github.com>
2025-03-19 03:54:07 +0800

    Add unique list

---
+++ src/main/java/tutorly/model/uniquelist/exceptions/ElementNotFoundException.java
@@ -0,0 +1,6 @@
+package tutorly.model.uniquelist.exceptions;
+
+/**
+ * Signals that the operation is unable to find the specified element.
+ */
+public class ElementNotFoundException extends RuntimeException {}
