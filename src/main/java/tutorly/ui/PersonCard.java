94caeccce54845aff33b9b8b301a2e06d58cedb8
Zhannyhong <33728815+Zhannyhong@users.noreply.github.com>
2025-03-22 14:37:42 +0800

    Display student ID as a Person field in UI

--- src/main/java/tutorly/ui/PersonCard.java
+++ src/main/java/tutorly/ui/PersonCard.java
@@ -29,9 +29,11 @@ public class PersonCard extends UiPart<Region> {
     @FXML
     private HBox cardPane;
     @FXML
+    private Label id;
+    @FXML
     private Label name;
     @FXML
-    private Label id;
+    private Label studentId;
     @FXML
     private Label phone;
     @FXML
@@ -50,6 +52,7 @@ public class PersonCard extends UiPart<Region> {
         super(FXML);
         this.person = person;
         id.setText(displayedIndex + ". ");
+        studentId.setText("Student ID: " + person.getId());
         name.setText(person.getName().fullName);
         phone.setText(person.getPhone().value);
         address.setText(person.getAddress().value);
